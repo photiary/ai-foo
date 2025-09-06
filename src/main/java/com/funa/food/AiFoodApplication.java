88c09de43dfe3c53dc4381d423b82d2c3cce3da9
funa <photiary@gmail.com>
2025-09-06 17:59:04 +0900

    init

---
+++ src/main/java/com/funa/food/AiFoodApplication.java
@@ -0,0 +1,13 @@
+package com.funa.food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiFoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/funa/food/AiFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.funa.food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
